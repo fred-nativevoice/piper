d5bf6813522dadf138906aa5b0bd1f43f0899217
fred-nativevoice <foliveira@nativevoice.ai>
2025-08-05 16:54:00 -0300

    Update
    Update to use ARPAbet phonemes.

--- src/python/piper_train/symbols.py
+++ src/python/piper_train/symbols.py
@@ -4,15 +4,16 @@ Defines the set of symbols used in text input to the model.
 """
 _bos = "^"
 _eos = "$"
-_pad = "_"
-_punctuation = ';:,.!?"\'’- '
-_letters = "abcdefghijklmnopqrstuvwxyz"
+_pad = "%"
+_numerals = "1234567890"
+_punctuation = ';:,.!?"\'-_\n '
+_letters = "#@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 _letters_ipa = (
     "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 )
 
 # Export all symbols:
-symbols = [_bos] + [_eos] + [_pad] + list(_punctuation) + list(_letters)
+symbols = [_pad] + [_bos] + [_eos] + list(_numerals) + list(_punctuation) + list(_letters)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
